56a687be0058f73595f4813a5c2df69e801ddbae
julian25-crypto <trisha.bajpai.2025@sse.ac.in>
2025-12-15 13:33:55 +0530

    Create playlist.py

---
+++ playlist.py
@@ -0,0 +1,21 @@
+print("WELCOME TO Trisha's Playlist")
+print("=" * 20)
+
+liked_songs = ["night changes","what makes you beautiful" ,"baby" , "belong together" , "all around the world" ]
+print(liked_songs)
+print("=" * 80)
+
+add_item = "strong"
+liked_songs.append(add_item)
+print(liked_songs)
+print("=" * 100)
+
+add_item = "perfect"
+liked_songs.append(add_item)
+print(liked_songs)
+print("=" * 115)
+
+remove_item = "baby"
+liked_songs.remove(remove_item)
+print(liked_songs)
+print("=" * 110)
